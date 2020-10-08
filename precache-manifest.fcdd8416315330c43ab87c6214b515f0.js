a38010f9de7463366f200b2a48c98fe93bb779fe
kamooni <tjrghks96@naver.com>
2020-10-08 15:56:30 +0900

    Updates

---
+++ precache-manifest.fcdd8416315330c43ab87c6214b515f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224fb5327ef1edf45b5ff3cdd6ab2931",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "c864f6d0630d87a208da",
+    "url": "/habit_tracker/static/css/main.6c673bc6.chunk.css"
+  },
+  {
+    "revision": "b5f20f3a78348fc3a1c3",
+    "url": "/habit_tracker/static/js/2.e07c0dca.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit_tracker/static/js/2.e07c0dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c864f6d0630d87a208da",
+    "url": "/habit_tracker/static/js/main.238e9f75.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
